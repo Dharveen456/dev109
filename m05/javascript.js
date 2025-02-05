eb649de95281f4151f78fe870aca00ca177b5738
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-02-05 14:51:53 -0800

    Update javascript.js

--- m05/javascript.js
+++ m05/javascript.js
@@ -3,19 +3,19 @@ var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '';              // Message
 
-if (operator === 'addition') {
+/*if (operator === 'addition') {
   // Do addition
   while (i < 11) {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
     i++;
   }
-} else {
+} else {*/
   // Do multiplication
   msg = '<h2>Multiplication</h2>';
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
-  }
+//  }
 }
 
 // Write the message into the page

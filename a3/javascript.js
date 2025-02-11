5b500edc0416c9542e87d6d2f4c328965830c228
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-02-11 12:01:25 -0800

    Update javascript.js

--- a3/javascript.js
+++ a3/javascript.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const symbol = document.getElementById("symbol");
     const height = document.getElementById("height");
     const submit = document.getElementById("submit");
-    const rhombus = document.getElementById("rhobmus");
+    const rhombus = document.getElementById("rhombus");
     submit.addEventListener("click", function(){
        rhombus.textContent = "jimmy johns";
     });

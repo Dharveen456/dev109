5c543e324b8b4a287be354c838935a4f226d26b1
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-02-11 14:02:58 -0800

    Update javascript.js

--- a3/javascript.js
+++ a3/javascript.js
@@ -14,6 +14,6 @@ document.addEventListener("DOMContentLoaded", function() {
             }
             rhombtext+="<br>";
         }
-       rhombus.textContent = rhombtext;
+       rhombus.innerHTML = rhombtext;
     });
 });

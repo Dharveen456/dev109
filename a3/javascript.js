f9f4103afb236ca6ce3d5413bb5a3e2625e88398
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-02-11 11:15:03 -0800

    Update javascript.js

--- a3/javascript.js
+++ a3/javascript.js
@@ -1,1 +1,6 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+    const color = document.getElementById("color");
+    const symbol = document.getElementById("symbol");
+    const height = document.getElementById("height");
+    const submit = document.getElementById("submit");
+});

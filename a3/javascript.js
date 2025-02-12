e2ec524798586741df3962290d1b55ce0b233746
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-02-12 09:28:10 -0800

    Update javascript.js

--- a3/javascript.js
+++ a3/javascript.js
@@ -9,10 +9,12 @@ document.addEventListener("DOMContentLoaded", function() {
         rhombus.style.lineheight = parseInt(height.value) + "px";
         let rhombtext="";
         let odd=true;
+        span.style.color = "#FF0000";
+        p.style.color = "#00FF00";
         for(let i=0; i<10; i++){
             for(let j=0; j<10; j++){
                 odd = !odd;
-                rhombtext+= odd ? "<span id='odd'>"+symbol.value+"</span>" :  symbol.value;
+                rhombtext+= odd ? "<span>"+symbol.value+"</span>" :  symbol.value;
             }
             rhombtext+="<br>";
         }

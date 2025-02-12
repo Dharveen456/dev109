f741055c6448feb005c7c1ddca5b7f56a301d495
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-02-12 09:07:19 -0800

    Update javascript.js

--- a3/javascript.js
+++ a3/javascript.js
@@ -6,10 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const submit = document.getElementById("submit");
     const rhombus = document.getElementById("rhombus");
     submit.addEventListener("click", function(){
-        rhombus.style.lineheight = height.value + "px";
+        rhombus.style.lineheight = parseInt(height.value) + "px";
         let rhombtext="";
         for(let i=0; i<10; i++){
-            for(let i=0; i<10; i++){
+            for(let j=0; j<10; j++){
                 rhombtext+=symbol.value;
             }
             rhombtext+="<br>";

15def3ad5596e4dd2aa11c9f1a11b36cb1dcfd33
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-02-12 13:51:43 -0800

    Update javascript.js

--- a3/javascript.js
+++ a3/javascript.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
         let rhombtext="";
         let evenColor = color2.value;
         let oddColor = color1.value;
-
         for (let i = 0; i < 10; i++) {
             for (let j = 0; j < 10; j++) {
                 odd = !odd;

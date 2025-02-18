c1d481bff2ceeb748ce382cc8bcf0046026ce9ee
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-02-18 09:20:30 -0800

    Update javascript.js

--- m05/javascript.js
+++ m05/javascript.js
@@ -2,14 +2,6 @@ var table = prompt("What unit do you want for the table");// Unit of table
 var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '';              // Message
-
-/*if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {*/
   // Do multiplication
   msg = '<h2>Multiplication</h2>';
   while (i < 11) {
@@ -17,7 +9,6 @@ var msg = '';              // Message
     i++;
 //  }
 }
-
 // Write the message into the page
 var el = document.getElementById('blackboard');
 el.innerHTML = msg;

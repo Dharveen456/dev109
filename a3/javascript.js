6daf97fc28e0dd39fc18c23a4f13b2d6d2d58968
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-02-22 20:47:54 -0800

    Update javascript.js

--- a3/javascript.js
+++ a3/javascript.js
@@ -6,13 +6,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const submit = document.getElementById("submit");
     const rhombus = document.getElementById("rhombus");
     submit.addEventListener("click", function(){
-        rhombus.style.lineHeight = parseInt(height.value) + "px";
         let rhombtext="";
         let evenColor = color2.value;
         let oddColor = color1.value;
         let odd = true;
-        for (let i = 1; i <= 10; i++) { //top half
-            for (let k = 0; k < 10-i; k++) {
+        for (let i = 1; i <= height.value; i++) { //top half
+            for (let k = 0; k < height.value-i; k++) {
                 rhombtext += `<span style="color:white">${symbol.value}</span>`;
             }
             for (let j = 0; j < 2*i; j++) {
@@ -21,16 +20,16 @@ document.addEventListener("DOMContentLoaded", function() {
                     ? `<span style="color:${oddColor}">${symbol.value}</span>` 
                     : `<span style="color:${evenColor}">${symbol.value}</span>`;
             }
-            for (let k = 0; k < 10-i; k++) {
+            for (let k = 0; k < height.value-i; k++) {
                 rhombtext += `<span style="color:white">${symbol.value}</span>`;
             }
             rhombtext += "<br>";
         }
-        for (let i = 0; i < 10; i++) { //bottom half
+        for (let i = 0; i < height.value; i++) { //bottom half
             for (let k = 0; k < i; k++) {
                 rhombtext += `<span style="color:white">${symbol.value}</span>`;
             }
-            for (let j = 0; j < 2*(10-i); j++) {
+            for (let j = 0; j < 2*(height.value-i); j++) {
                 odd = !odd;
                 rhombtext += odd 
                     ? `<span style="color:${oddColor}">${symbol.value}</span>` 

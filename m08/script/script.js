8a25569cb60ae277abf84a3596e9d54e734f8e42
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-02-25 10:25:52 -0800

    Update script.js

--- m08/script/script.js
+++ m08/script/script.js
@@ -1,17 +1,20 @@
-const button = document.getElementByTag("button")[0];
-/*button.addEventListener("click", function(){
-  // Create a new element and store it in a variable.
-  var newEl = document.createElement('li');
+document.addEventListener("DOMContentLoaded", function() {
+  const button = document.getElementByTag("button")[0];
+  const input = document.getElementByTag("input")[0];
+  button.addEventListener("click", function(){
+    // Create a new element and store it in a variable.
+    var newEl = document.createElement('li');
 
-  // Create a text node and store it in a variable.
-  var newText = document.createTextNode('quinoa');
+    // Create a text node and store it in a variable.
+    var newText = document.createTextNode(input.value);
 
-  // Attach the new text node to the new element.
-  newEl.appendChild(newText);
+    // Attach the new text node to the new element.
+    newEl.appendChild(newText);
 
-  // Find the position where the new element should be added.
-  var position = document.getElementsByTagName('ul')[0];
+    // Find the position where the new element should be added.
+    var position = document.getElementsByTagName('ul')[0];
 
-  // Insert the new element into its position.
-  position.appendChild(newEl);
-}*/
+    // Insert the new element into its position.
+    position.appendChild(newEl);
+  }
+}

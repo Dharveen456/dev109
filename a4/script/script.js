1e72302dc2ed88a1c917aedb520c7f04ac0d86db
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-02-26 10:25:29 -0800

    Update script.js

--- a4/script/script.js
+++ a4/script/script.js
@@ -13,9 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
   const zipcode = getElementById("zipcode");
   const comments = getElementById("comments");
   submit.addEventListener("click", function(){
-  if (firstname.value==="null" || firstname.value==="" || firstname.value.length > 20)
-    errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
-  
-
-  });
+    errorMessages += (firstname.value==="null" || firstname.value==="" || firstname.value.length > 20) ? "The firstname is required and cannot be greater than 20 characters<br>";
+    errorMessages += (/^[A-Za-z]+$/.test(firstname.value)) ? "The firstname is must only include alphabetic characters<br>";
+    errorMessages += (lastname.value==="null" || lastname.value==="" || lastname.value.length > 20) ? "The lastname is required and cannot be greater than 20 characters<br>";
+    let atpos = email.value.indexOf("@");
+    let dotpos = email.value.lastIndexOf(".");
+    errorMessages += (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) ? "Invalid email<br>";
+    errorMessages += (isNaN(phonenumber.value) || phonenumber.value.lenght >15 || phonenumber.value===null || phonenumber.value==="") ? "Invalid phone number<br>";
+    errorMessages += (username.value==="null" || username.value==="" || username.value.length > 12) ? "The lastname is required and cannot be greater than 20 characters<br>";
 });

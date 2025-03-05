0de9b2e6b5f108ca0cb3dc98ca343b413570323a
Dharveen456 <dharveen.suntheresen@bellevuecollege.edu>
2025-03-05 09:07:47 -0800

    Update script.js

--- m09/js/script.js
+++ m09/js/script.js
@@ -1,22 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-var  mybutton = document.querySelector("button");
-mybutton.addEventListener("click", function(event) {
-
-
-var element = document.getElementsByTagName("div");
-for (index = element.length - 1; index >= 0; index--) {
-    element[index].parentNode.removeChild(element[index]);
-}
-
-// Let us stop the propagation of events
-
-event.stopPropagation();
-  });
-  addEventListener("click", function(event) {
-    var dot = document.createElement("div");
-    dot.className = "dot";
-    dot.style.left = (event.pageX - 4) + "px";
-    dot.style.top = (event.pageY - 4) + "px";
-    document.body.appendChild(dot);
-  });
-});
+            var mybutton = document.querySelector("button");
+            mybutton.addEventListener("click", function(event) {
+                var elements = document.getElementsByTagName("div");
+                for (var index = elements.length - 1; index >= 0; index--) {
+                    elements[index].parentNode.removeChild(elements[index]);
+                }
+                event.stopPropagation();
+            });
+            document.addEventListener("click", function(event) {
+                var dot = document.createElement("div");
+                dot.className = "dot";
+                dot.style.left = (event.pageX - 4) + "px";
+                dot.style.top = (event.pageY - 4) + "px";
+                document.body.appendChild(dot);
